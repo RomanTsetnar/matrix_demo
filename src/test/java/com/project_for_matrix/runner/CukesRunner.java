8d0b419df143fb030c94ce32f9a8f11e4c2af534
romantsetnar <r.tsetnar@gmail.com>
2022-11-22 14:02:03 -0600

    refactored some info

---
+++ src/test/java/com/project_for_matrix/runner/CukesRunner.java
@@ -0,0 +1,21 @@
+package com.ad_ho_research.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "rerun:target/rerun.txt", "json:target/cucumber-report.json"},
+        features = "src/test/resources/features",
+        glue = "com/ad_ho_research/step_deffs",
+        dryRun = false,
+        tags = "@test3"
+
+)
+public class CukesRunner {
+
+}
